6d7161ea791e4271753045560022337136237da8
Daggie Blanqx <daggieblanqx@gmail.com>
2018-12-13 14:26:43 +0300

    Updated some files , fixed some bugs

--- js/app.js
+++ js/app.js
@@ -2,5 +2,43 @@
 
 const app = {
 	name:'Circle K Solutions',
-	url : '//localhost:3000'
+	env : 'dev',
+	url : function (){
+		if (app.env == 'dev') {
+		return '//localhost:3000';	
+		}else{
+			return '//circleksolutions.com';
+		}
+	}
 }
+
+
+
+/*
+START : MATERIALIZE INITIALIZATIONS
+*/
+
+//navbar dropdown
+$(".dropdown-trigger").dropdown({ hover: true });
+
+//navbar mobile
+$('.sidenav').sidenav();
+
+//tabs
+$('.tabs').tabs({
+	swipeable:true
+});
+
+//feature discovery
+$('.tap-target').tapTarget();
+
+//init modal
+$('.modal').modal();
+
+//init form select
+$('select').formSelect();
+
+
+/*
+END : MATERIALIZE INITIALIZATIONS
+*/
